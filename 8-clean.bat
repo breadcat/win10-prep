eaff701b2f3e1bff2f4290ad2fb12c567130b98d
breadcat <peter@minskio.co.uk>
2019-02-13 19:44:47 +0000

    Improve clean script

--- 8-clean.bat
+++ 8-clean.bat
@@ -1,3 +1,7 @@
 rem clean up
 if exist "%userprofile%\Desktop\*.lnk" del "%userprofile%\Desktop\*.lnk"
 if exist "%allusersprofile%\Desktop\*.lnk" del "%allusersprofile%\Desktop\*.lnk"
+if exist "%userprofile%\Desktop\*.appref-ms" del "%userprofile%\Desktop\*.appref-ms"
+if exist "%allusersprofile%\Desktop\*.appref-ms" del "%allusersprofile%\Desktop\*.appref-ms"
+if exist "%public%\Desktop\*.lnk" del "%public%\Desktop\*.lnk"
+if exist "%public%\Desktop\*.lnk" del "%public%\Desktop\*.lnk"
