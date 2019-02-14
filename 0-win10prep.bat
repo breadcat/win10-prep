ab7e10978d50bf24bbd11bb3953e205d60f0eafd
breadcat <peter.gill@chillitelecom.co.uk>
2019-02-14 16:48:04 +0000

    Fix broken call operations

--- 0-win10prep.bat
+++ 0-win10prep.bat
@@ -15,20 +15,38 @@ echo.
 set /p=
 
 rem Process
-<NUL set /p= :: Activating Windows && call 1-activation.bat > nul && echo  ...done!
-<NUL set /p= :: System Tweaks && call 2-tweaks.bat > nul && echo  ...done!
-<NUL set /p= :: Registry Tweaks && call 3-tweaks.bat > nul && echo  ...done!
-<NUL set /p= :: Removing Programs && call 4-remove.bat > nul && echo  ...done!
-<NUL set /p= :: Install Programs && call 5-install.bat > nul && echo  ...done!
-<NUL set /p= :: Mounting Network && call 6-network.bat > nul && echo  ...done!
-<NUL set /p= :: Windows Updates && call 7-install.bat > nul && echo  ...done!
-<NUL set /p= :: Clean Up && call 8-clean.bat > nul && echo  ...done!
+<NUL set /p=:: Activating Windows... 
+call "1-activation.bat" > nul
+echo done
+<NUL set /p=:: System Tweaks... 
+call "2-tweaks.bat" > nul
+echo done
+<NUL set /p=:: Registry Tweaks... 
+call "3-tweaks.bat" > nul
+echo done
+<NUL set /p=:: Removing Programs... 
+call "4-remove.bat" > nul
+echo done
+<NUL set /p=:: Install Programs... 
+call "5-install.bat" > nul
+echo done
+<NUL set /p=:: Mounting Network... 
+call "6-network.bat" > nul
+echo done
+<NUL set /p=:: Windows Updates... 
+call "7-install.bat" > nul
+echo done
+<NUL set /p=:: Clean Up... 
+call "8-clean.bat" > nul
+echo done
 
 rem complete
-cls
 echo.
-echo Script complete, restart whenever you fancy.
-echo Press [enter] to exit.
-set /P=
+echo  :: Script complete
+echo.
+echo     Restart whenever you fancy.
+echo     Press [enter] to exit.
+echo.
+set /p=
 :eof
 exit
