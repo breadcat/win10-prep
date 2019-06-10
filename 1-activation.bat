db5bb1c3c6c0316d17ba6f080d915b1f5261a16b
breadcat <peter.gill@chillitelecom.co.uk>
2019-06-10 08:53:12 +0100

    Improve activation
    Doesn't need a key, turns out

--- 1-activation.bat
+++ 1-activation.bat
@@ -1,5 +1,2 @@
-rem activate windows, requires local KMS server and windows 10 enterprise ltsc, no exceptions
-rem generic keys: https://docs.microsoft.com/en-us/windows-server/get-started/kmsclientkeys
-slmgr/ipk QFFDN-GRT3P-VKWWX-X7T3R-8B639
-ping -n 1 192.168.1.3 | find "TTL=" >nul 
-if errorlevel 0 ( slmgr/skms 192.168.1.3 && slmgr/ato ) else ( echo no local kms server found)
+ping -n 1 minskio.co.uk | find "TTL=" >nul 
+if errorlevel 0 ( slmgr/skms minskio.co.uk && slmgr/ato ) else ( echo server did not respond to ping requests)
